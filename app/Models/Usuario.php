a79fdd493af98e333faf548b759a72244a9a6114
ReyesMacias02 <jjrm1351828189@gmail.com>
2024-08-19 17:30:56 -0500

    usuario

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -4,15 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Facades\Storage;
 
 class Usuario extends Model
 {
     use SoftDeletes;
 
     protected $table = 'usuario';
-
-
-
 
 }
